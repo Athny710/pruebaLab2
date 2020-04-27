9d180f0c1f3673e5372627836a5ef00a287cdec7
Anthony Rojas <a20145976@pucp.pe>
2020-04-27 15:26:11 -0500

    baselab

---
+++ src/main/java/com/example/practicalab2/Practicalab2Application.java
@@ -0,0 +1,13 @@
+package com.example.practicalab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicalab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practicalab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicalab2/Practicalab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicalab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
